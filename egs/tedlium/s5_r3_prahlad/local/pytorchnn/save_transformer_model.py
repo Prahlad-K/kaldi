2dc03f42657f5aa825faa6c96ce605a677471917
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-05 04:37:55 -0500

    updating model to save with sample inputs

--- egs/tedlium/s5_r3_prahlad/local/pytorchnn/save_transformer_model.py
+++ egs/tedlium/s5_r3_prahlad/local/pytorchnn/save_transformer_model.py
@@ -1,6 +1,19 @@
 from transformers import TransfoXLTokenizer, TransfoXLModel
 import torch
 
+tokenizer = TransfoXLTokenizer.from_pretrained("transfo-xl-wt103")
 model = TransfoXLModel.from_pretrained("transfo-xl-wt103")
-traced_model = torch.jit.trace(model)
+
+
+# Tokenizing input text
+tokenized_text  = tokenizer("Hello, my dog is cute", return_tensors="pt")
+
+# Masking one of the input tokens
+indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+# Creating a dummy input
+tokens_tensor = torch.tensor([indexed_tokens])
+
+# Creating the trace
+traced_model = torch.jit.trace(model, tokens_tensor)
 torch.jit.save(traced_model, "~/exp/pytorch_transformer/")
