0daff64c5b071fa2fef42309c55ce4e1b8123e58
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-05 05:21:18 -0500

    Correcting path to decoded files

--- egs/tedlium/s5_r3_prahlad/run_transformerXL_LM.sh
+++ egs/tedlium/s5_r3_prahlad/run_transformerXL_LM.sh
@@ -235,7 +235,7 @@ if [ $stage -le 19 ]; then
 
   for dset in dev test; do
     data_dir=data/${dset}_hires
-    decoding_dir=exp/chain_cleaned/tdnnf_1a/decode_${dset}
+    decoding_dir=exp/chain_cleaned_1d/tdnn1d_sp/decode_${dset}
     suffix=$(basename $tnnlm_dir)
     output_dir=${decoding_dir}_$suffix
     
