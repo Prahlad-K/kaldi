444ed5d6373ed1cff20b32f2fc1e7506f0562efb
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-05 19:19:12 -0500

    Not enforcing nnmodel to be a file

--- egs/wsj/s5/steps/pytorchnn/lmrescore_lattice_pytorchnn-XL.sh
+++ egs/wsj/s5/steps/pytorchnn/lmrescore_lattice_pytorchnn-XL.sh
@@ -62,7 +62,7 @@ elif [ ! -f $oldlm ]; then
     exit 1;
 fi
 
-for f in $nn_model $vocab $indir/lat.1.gz; do
+for f $vocab $indir/lat.1.gz; do
   [ ! -f $f ] && echo "$0: expected file $f to exist." && exit 1;
 done
 
