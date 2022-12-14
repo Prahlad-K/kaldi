01b9d3517bb00de15bec112c765a281c5b0aaae8
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-14 01:22:14 -0500

    Added required dones

--- egs/librispeech/s5/run_test.sh
+++ egs/librispeech/s5/run_test.sh
@@ -74,6 +74,7 @@ if [ $stage -le 4 ]; then
   #   data/lang_nosp data/lang_nosp_test_tglarge
   # utils/build_const_arpa_lm.sh data/local/lm/lm_fglarge.arpa.gz \
   #   data/lang_nosp data/lang_nosp_test_fglarge
+  done
 fi
 
 if [ $stage -le 5 ]; then
@@ -83,6 +84,7 @@ if [ $stage -le 5 ]; then
   #   utils/create_split_dir.pl /export/b{02,11,12,13}/$USER/kaldi-data/egs/librispeech/s5/$mfcc/storage \
   #    $mfccdir/storage
   # fi
+  done
 fi
 
 
@@ -102,12 +104,14 @@ if [ $stage -le 7 ]; then
   # utils/subset_data_dir.sh --shortest data/train_clean_100 2000 data/train_2kshort
   # utils/subset_data_dir.sh data/train_clean_100 5000 data/train_5k
   # utils/subset_data_dir.sh data/train_clean_100 10000 data/train_10k
+  done
 fi
 
 if [ $stage -le 8 ]; then
   # train a monophone system
   # steps/train_mono.sh --boost-silence 1.25 --nj 20 --cmd "$train_cmd" \
   #                     data/train_2kshort data/lang_nosp exp/mono
+  done
 fi
 
 if [ $stage -le 9 ]; then
@@ -117,6 +121,7 @@ if [ $stage -le 9 ]; then
   # train a first delta + delta-delta triphone system on a subset of 5000 utterances
   # steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" \
   #                       2000 10000 data/train_5k data/lang_nosp exp/mono_ali_5k exp/tri1
+  done
 fi
 
 if [ $stage -le 10 ]; then
@@ -128,6 +133,7 @@ if [ $stage -le 10 ]; then
   # steps/train_lda_mllt.sh --cmd "$train_cmd" \
   #                         --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
   #                         data/train_10k data/lang_nosp exp/tri1_ali_10k exp/tri2b
+  done
 fi
 
 if [ $stage -le 11 ]; then
@@ -138,7 +144,7 @@ if [ $stage -le 11 ]; then
   # # Train tri3b, which is LDA+MLLT+SAT on 10k utts
   # steps/train_sat.sh --cmd "$train_cmd" 2500 15000 \
   #                    data/train_10k data/lang_nosp exp/tri2b_ali_10k exp/tri3b
-
+  done
 fi
 
 if [ $stage -le 12 ]; then
@@ -151,6 +157,7 @@ if [ $stage -le 12 ]; then
   # steps/train_sat.sh  --cmd "$train_cmd" 4200 40000 \
   #                     data/train_clean_100 data/lang_nosp \
   #                     exp/tri3b_ali_clean_100 exp/tri4b
+  done
 fi
 
 if [ $stage -le 13 ]; then
@@ -171,6 +178,7 @@ if [ $stage -le 13 ]; then
   #   data/local/lm/lm_tglarge.arpa.gz data/lang data/lang_test_tglarge
   # utils/build_const_arpa_lm.sh \
   #   data/local/lm/lm_fglarge.arpa.gz data/lang data/lang_test_fglarge
+  done
 fi
 
 if [ $stage -le 14 ] && false; then
@@ -182,6 +190,7 @@ if [ $stage -le 14 ] && false; then
 
   # # This nnet2 training script is deprecated.
   # local/nnet2/run_5a_clean_100.sh
+  done
 fi
 
 if [ $stage -le 15 ]; then
@@ -198,6 +207,7 @@ if [ $stage -le 15 ]; then
   # # ... and then combine the two sets into a 460 hour one
   # utils/combine_data.sh \
   #   data/train_clean_460 data/train_clean_100 data/train_clean_360
+  done
 fi
 
 if [ $stage -le 16 ]; then
@@ -208,6 +218,7 @@ if [ $stage -le 16 ]; then
   # # create a larger SAT model, trained on the 460 hours of data.
   # steps/train_sat.sh  --cmd "$train_cmd" 5000 100000 \
   #                     data/train_clean_460 data/lang exp/tri4b_ali_clean_460 exp/tri5b
+  done
 fi
 
 
@@ -231,6 +242,7 @@ if [ $stage -le 17 ]; then
   # # combine all the data
   # utils/combine_data.sh \
   #   data/train_960 data/train_clean_460 data/train_other_500
+  done
 fi
 
 if [ $stage -le 18 ]; then
@@ -264,6 +276,7 @@ if [ $stage -le 19 ]; then
   # this does some data-cleaning. The cleaned data should be useful when we add
   # the neural net and chain systems.  (although actually it was pretty clean already.)
   # local/run_cleanup_segmentation.sh
+  done
 fi
 
 # steps/cleanup/debug_lexicon.sh --remove-stress true  --nj 200 --cmd "$train_cmd" data/train_clean_100 \
@@ -290,6 +303,7 @@ fi
 if [ $stage -le 20 ]; then
   # train and test nnet3 tdnn models on the entire data with data-cleaning.
   # local/chain/run_tdnn.sh # set "--stage 11" if you have already run local/nnet3/run_tdnn.sh
+  done
 fi
 
 # The nnet3 TDNN recipe:
