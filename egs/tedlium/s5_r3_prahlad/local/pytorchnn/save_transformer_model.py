a405ebcfae431b8649f350a2a01c3dec27d36648
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-15 11:43:43 -0500

    n-best

--- egs/tedlium/s5_r3_prahlad/local/pytorchnn/save_transformer_model.py
+++ egs/tedlium/s5_r3_prahlad/local/pytorchnn/save_transformer_model.py
@@ -1,7 +1,7 @@
-from transformers import TransfoXLTokenizer, TransfoXLModel
+from transformers import TransfoXLLMHeadModel
 import torch
 
 # Get the model online
-model = TransfoXLModel.from_pretrained("transfo-xl-wt103")
+model = TransfoXLLMHeadModel.from_pretrained("transfo-xl-wt103")
 # Save the pretrained model
-TransfoXLModel.save_pretrained(model, 'exp/transformer_xl/')
+TransfoXLLMHeadModel.save_pretrained(model, 'exp/transformer_xl/')
