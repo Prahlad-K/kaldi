1faed4370df89ec58c66d48ed6985bfdb99e7db5
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-15 03:26:26 -0500

    filepath

--- egs/tedlium/s5_r3_prahlad/local/pytorchnn/train_gru_run.sh
+++ egs/tedlium/s5_r3_prahlad/local/pytorchnn/train_gru_run.sh
@@ -4,5 +4,4 @@
 [ -z "$cmd" ] && cmd=$train_cmd
 nj=8
 echo "$0: training gcnn."
-$train_cmd JOB=1:$nj log/train_gru.JOB.log \
-PYTHONPATH=local/pytorchnn python3 train_gru.py
+$train_cmd JOB=1:$nj log/train_gru.JOB.log python3 local/pytorchnn/train_gru.py
