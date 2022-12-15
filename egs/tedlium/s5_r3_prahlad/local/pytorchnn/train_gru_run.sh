471a0d00d8e4995c79cd39b2b717a1dc5ea14d27
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-15 03:18:15 -0500

    update in paths

--- egs/tedlium/s5_r3_prahlad/local/pytorchnn/train_gru_run.sh
+++ egs/tedlium/s5_r3_prahlad/local/pytorchnn/train_gru_run.sh
@@ -1,5 +1,7 @@
-cmd=run.pl
+. ./../../cmd.sh
+. ./../../path.sh
+
 nj=8
 echo "$0: computing neural LM scores of the minimal list of hypotheses."
-$cmd JOB=1:$nj $dir/log/train_gru.JOB.log \
+$train_cmd JOB=1:$nj log/train_gru.JOB.log \
 PYTHONPATH=steps/pytorchnn python3 train_gru.py
