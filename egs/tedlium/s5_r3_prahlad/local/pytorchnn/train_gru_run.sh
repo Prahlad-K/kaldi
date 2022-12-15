b3e6d08f0fd08b34df4f2dbc805e2982c3e22bff
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-15 03:21:47 -0500

    corrected traincmd

--- egs/tedlium/s5_r3_prahlad/local/pytorchnn/train_gru_run.sh
+++ egs/tedlium/s5_r3_prahlad/local/pytorchnn/train_gru_run.sh
@@ -1,7 +1,8 @@
-. ./../../cmd.sh
-. ./../../path.sh
-
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
+[ -z "$cmd" ] && cmd=$train_cmd
 nj=8
-echo "$0: computing neural LM scores of the minimal list of hypotheses."
+echo "$0: training gcnn."
 $train_cmd JOB=1:$nj log/train_gru.JOB.log \
 PYTHONPATH=steps/pytorchnn python3 train_gru.py
