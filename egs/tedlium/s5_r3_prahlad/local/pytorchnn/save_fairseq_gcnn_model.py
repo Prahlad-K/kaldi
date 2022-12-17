de584c032e5d8f038f727c8edbcf86b450ee6093
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-17 15:32:03 -0500

    Updated results + gcnnlm code

--- egs/tedlium/s5_r3_prahlad/local/pytorchnn/save_fairseq_gcnn_model.py
+++ egs/tedlium/s5_r3_prahlad/local/pytorchnn/save_fairseq_gcnn_model.py
@@ -1,6 +1,6 @@
 from transformers import BertLMHeadModel
 
 # Get the model online
-model = BertLMHeadModel.from_pretrained("bert-base-cased")
+model = BertLMHeadModel.from_pretrained("bert-base-uncased", is_decoder=True)
 # Save the pretrained model
 BertLMHeadModel.save_pretrained(model, 'exp/gcnnlm/')
