f96ae1b58d505b73af5a80c4c19ba9e8a2d5b06e
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-18 11:06:28 -0500

    Adding GCNNLM into results

--- egs/tedlium/s5_r3_prahlad/results.sh
+++ egs/tedlium/s5_r3_prahlad/results.sh
@@ -6,7 +6,7 @@ filter_regexp=.
 [ $# -ge 1 ] && filter_regexp=$1
 
 # kaldi scoring,
-for x in exp/{mono,tri,sgmm,nnet,dnn,lstm,pytorch_transformer,transformer_xl}*/decode*; do
+for x in exp/{mono,tri,sgmm,nnet,dnn,lstm,pytorch_transformer,transformer_xl,gcnnlm}*/decode*; do
   [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh;
 done 2>/dev/null
 for x in exp/chain*/*/decode*; do
@@ -14,7 +14,7 @@ for x in exp/chain*/*/decode*; do
 done 2>/dev/null | grep $filter_regexp
 
 # sclite scoring,
-for x in exp/{mono,tri,sgmm,nnet,dnn,lstm,pytorch_transformer,transformer_xl}*/decode*; do
+for x in exp/{mono,tri,sgmm,nnet,dnn,lstm,pytorch_transformer,transformer_xl,gcnnlm}*/decode*; do
   [ -d $x ] && grep Sum $x/score_*/*.sys | utils/best_wer.sh;
 done 2>/dev/null | grep $filter_regexp
 for x in exp/chain*/*/decode*; do
