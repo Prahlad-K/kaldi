d5e53f248ac72fb8167ef078083b99e502b5dbc2
Prahlad Koratamaddi <prahladsharmak99@gmail.com>
2022-12-17 23:39:49 -0500

    Running decoding from scratch cause you need to

--- egs/tedlium/s5_r3_prahlad/run_pk2743.sh
+++ egs/tedlium/s5_r3_prahlad/run_pk2743.sh
@@ -322,7 +322,8 @@ if [ $stage -le 19 ]; then
         --nhead $nhead \
         --weight 0.7 \
         --oov-symbol "'$oov'" \
-        --stage 7 \
+        #--stage 7 \
+        --stage 0 \
         $lang_dir $nn_model $vocab_data_dir/words.txt \
         $data_dir $decoding_dir \
         $output_dir
@@ -339,7 +340,8 @@ if [ $stage -le 19 ]; then
         --beam 4 \
         --epsilon 0.5 \
         --oov-symbol "'$oov'" \
-        --stage 3 \
+        #--stage 3 \
+        --stage 1 \
         $lang_dir $nn_model $vocab_data_dir/words.txt \
         $data_dir $decoding_dir \
         $output_dir
